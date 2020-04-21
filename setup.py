eb6d2cf65388832522bffb443961e2644cd534af
Omri Rozenzaft <omri.rozenzaft@gm.com>
2020-04-21 09:43:23 +0300

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pyrosenv',
-    version='0.0.2',
+    version='0.0.3',
     author='Omri Rozenzaft',
     author_email='omrirz@gmail.com',
     url='https://github.com/omrirz/pyrosenv.git',
